eea442e8da6bb0815d5c7fa9ad78a70f6ea6f86a
Bryan Gale <bryan@bryangale.com>
2024-02-02 15:57:50 +0000

    Only update AAAA record when IPv6 address has changed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use reqwest::StatusCode;
+use serde_json::Value;
 use std::{collections::HashMap, str::FromStr, time::Duration};
 use tokio::time::sleep;
 
@@ -20,8 +22,31 @@ async fn main() {
 
     let client: reqwest::Client = reqwest::Client::new();
 
+    let record_url = format!(
+        "https://api.linode.com/v4/domains/{}/records/{}",
+        cli.domain_id, cli.record_id
+    );
+
+    let record_json = client
+        .get(&record_url)
+        .bearer_auth(&cli.token)
+        .send()
+        .await
+        .unwrap()
+        .json::<HashMap<String, Value>>()
+        .await
+        .unwrap();
+
+    let mut current_ip = (if let Value::String(target) = record_json.get("target").unwrap() {
+        Some(target)
+    } else {
+        None
+    })
+    .unwrap()
+    .clone();
+
     loop {
-        let ipv6_address = client
+        let new_ip = client
             .get("https://api6.ipify.org")
             .send()
             .await
@@ -30,29 +55,35 @@ async fn main() {
             .await
             .unwrap();
 
-        println!("IPv6 address: {}", ipv6_address);
+        if new_ip != current_ip {
+            println!(
+                "IPv6 address changed from {} to {}, updating AAAA record...",
+                current_ip, new_ip
+            );
 
-        let put_url = format!(
-            "https://api.linode.com/v4/domains/{}/records/{}",
-            cli.domain_id, cli.record_id
-        );
+            let mut target_json = HashMap::new();
 
-        let mut target_json = HashMap::new();
+            target_json.insert(String::from_str("target").unwrap(), &new_ip);
 
-        target_json.insert(String::from_str("target").unwrap(), ipv6_address);
+            let result = client
+                .put(&record_url)
+                .bearer_auth(&cli.token)
+                .json(&target_json)
+                .send()
+                .await
+                .unwrap()
+                .status();
 
-        let result = client
-            .put(put_url)
-            .bearer_auth(&cli.token)
-            .json(&target_json)
-            .send()
-            .await
-            .unwrap()
-            .text()
-            .await
-            .unwrap();
+            if result == StatusCode::OK {
+                println!("AAAA record updated");
+            } else {
+                println!("Failed to update AAAA record");
+            }
 
-        println!("Result: {}", result);
+            current_ip = new_ip;
+        } else {
+            println!("IPv6 unchaged from {}", current_ip);
+        }
 
         sleep(Duration::from_secs(cli.delay as u64)).await;
     }
